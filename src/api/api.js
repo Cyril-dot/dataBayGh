329ed504b1a8440e5af215eb332dc38d8c2e65b7
Cyril-dot <chukwukaottih@gmail.com>
2026-07-09 17:04:46 +0000

    update

--- src/api/api.js
+++ src/api/api.js
@@ -30,7 +30,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://databaybackend-production.up.railway.app';
 
 /* ----------------------------------------------------------------------- */
 /*  Token storage (localStorage)                                           */
